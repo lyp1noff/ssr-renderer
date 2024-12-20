02950fdc917ca117ee703001096e7e7de0a97350
lyp1noff <lyp1noff@gmail.com>
2024-12-20 17:07:53 +0000

    working

--- dockerfile
+++ dockerfile
@@ -1,7 +1,29 @@
-FROM node:18-alpine
+FROM node:lts-alpine3.20
 WORKDIR /app
+RUN apk update && apk add bash
+
+RUN apk add --no-cache \
+    chromium \
+    nss \
+    freetype \
+    harfbuzz \
+    ca-certificates \
+    ttf-freefont \
+    nodejs
+
+ENV PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser
+
 COPY package*.json ./
-RUN npm install --production
+RUN npm install
+COPY . .
+
+RUN addgroup -S pptruser && adduser -S -G pptruser pptruser \
+    && mkdir -p /home/pptruser/Downloads /app \
+    && chown -R pptruser:pptruser /home/pptruser \
+    && chown -R pptruser:pptruser /app
+
+USER pptruser
+
 COPY . .
 EXPOSE 3000
 CMD ["npm", "start"]
--- ssr-server.js
+++ ssr-server.js
@@ -1,48 +1,84 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
+const express = require("express");
+const puppeteer = require("puppeteer");
 
-const app = express();
+function parseBoolStr(boolStr, defaultValue) {
+  return boolStr ? boolStr === "true" : defaultValue;
+}
 
-const BLOCKED_RESOURCE_TYPES = ['image', 'media', 'stylesheet', 'font'];
-
-
-app.get('*', async (req, res) => {
-    const targetUrl = `https://helen-marlen.com${req.originalUrl}`;
-
-    console.log(`Rendering: ${targetUrl}`);
-
-    let browser;
-    try {
-        browser = await puppeteer.launch({ headless: 'new' });
-        const page = await browser.newPage();
-
-        await page.setRequestInterception(true);
-        page.on('request', (request) => {
-            const resourceType = request.resourceType();
-            if (BLOCKED_RESOURCE_TYPES.includes(resourceType)) {
-                request.abort();
-            } else {
-                request.continue();
-            }
-        });
-
-        await page.goto(targetUrl, { waitUntil: 'networkidle2' });
-
-        const content = await page.content();
-
-        res.send(content);
-    } catch (error) {
-        console.error(`Failed to render ${targetUrl}:`, error);
-        res.status(500).send('Internal Server Error');
-    } finally {
-        if (browser) {
-            await browser.close();
-        }
-    }
-});
-
-// Запуск сервера
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`SSR server running on http://localhost:${PORT}`);
-});
+
+const LOG_WARNINGS = parseBoolStr(process.env.PSSR_LOG_WARNINGS, true);
+const LOG_INFO = parseBoolStr(process.env.PSSR_LOG_INFO, true);
+const LOG_DEBUG = parseBoolStr(process.env.PSSR_LOG_DEBUG, false);
+
+const info = LOG_INFO ? console.log : () => null;
+const debug = LOG_DEBUG ? console.log : () => null;
+const warn = LOG_WARNINGS ? console.warn : () => null;
+
+const BLOCKED_RESOURCE_TYPES = ["image", "media", "stylesheet", "font"];
+
+let currentBrowser;
+
+async function closeBrowser(browser) {
+    await new Promise(resolve => setTimeout(resolve, BROWSER_COOLDOWN_TIME));
+    const nOpenPages = (await browser.pages()).length - 1;
+    if (nOpenPages > 0) {
+      info(`Closing old browser with ${nOpenPages} open pages.`);
+    }
+    return browser.close();
+  }
+
+async function refreshBrowser() {
+    debug('Refreshing browser.');
+    const oldBrowser = currentBrowser;
+    // currentBrowser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
+    currentBrowser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-gpu', '--disable-setuid-sandbox']});
+    // currentBrowser = await puppeteer.launch({headless: true});
+    if (oldBrowser) {
+      await closeBrowser(oldBrowser);
+    }
+  }
+
+async function handleRequest(req, res) {
+  const targetUrl = `https://helen-marlen.com${req.originalUrl}`;
+
+  console.log(`Rendering: ${targetUrl}`);
+
+  try {
+    const page = await currentBrowser.newPage();
+
+    await page.setRequestInterception(true);
+    page.on("request", (request) => {
+      const resourceType = request.resourceType();
+      if (BLOCKED_RESOURCE_TYPES.includes(resourceType)) {
+        request.abort();
+      } else {
+        request.continue();
+      }
+    });
+
+    await page.goto(targetUrl, { waitUntil: "networkidle2" });
+
+    const content = await page.content();
+    page.close();
+
+    res.send(content);
+  } catch (error) {
+    console.error(`Failed to render ${targetUrl}:`, error);
+    res.status(500).send("Internal Server Error");
+  }
+}
+
+async function init() {
+  info("Starting.");
+  await refreshBrowser();
+  setInterval(refreshBrowser, 60000);
+  info(`Browser started, starting server.`);
+  const app = express();
+  app.get("*", handleRequest);
+  app.listen(PORT, () => {
+    info(`Now listening on port ${PORT}.`);
+  });
+}
+
+init();
